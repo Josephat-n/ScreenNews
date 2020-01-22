23fe8334ca83c9b4622519f5ad3b8e5f1edb182d
Josephat-n <josphatnjoroge254@gmail.com>
2020-01-22 16:56:35 +0300

    display titles from the currents news site

--- news/views.py
+++ news/views.py
@@ -3,10 +3,19 @@ from django.contrib import messages
 from .models import User
 from .forms import UserRegisterForm
 from django.contrib.auth.decorators import login_required
+import requests
 
 # Create your views here.
 def home(request):
-   return render(request,'news/home.html')
+   url = 'https://api.currentsapi.services/v1/search?keywords=Amazon&language=en&apiKey=dBdy9d2EsMbrOnT6nDA6ygdFEIy-qB98zMBzpfF77Sqy8BS2'
+   
+   response = requests.get(url).json()
+   # print (response.text)
+   context = {
+      'responses': response['news']
+   }
+      
+   return render(request,'news/home.html', context)
 
 def register(request):
    if request.method == 'POST':
